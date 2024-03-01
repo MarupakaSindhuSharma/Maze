50fe866d9c18d7885e43ef2bfc858c0341f3308c
MarupakaSindhuSharma <153704323+MarupakaSindhuSharma@users.noreply.github.com>
2024-03-01 20:25:47 +0530

    Add files via upload

---
+++ MazeWorld.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MazeWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MazeWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MazeWorld.
+     * 
+     */
+    public MazeWorld()
+    {
+        super(12,12,50);
+    }
+}
